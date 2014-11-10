46d53beab7f2fc648d72389ea5456b20eb37c434
Dipesh Acharya <xtranophilist@gmail.com>
2014-11-10 16:09:44 +0545

    users app

---
+++ users/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import login
+from django.contrib.auth import logout as auth_logout
+
+
+def index(request):
+    if request.user.is_authenticated():
+        return render(request, 'dashboard_index.html')
+        return render(request, 'dashboard_index.html')
+    return login(request)
+
+
+def web_login(request, **kwargs):
+    if request.user.is_authenticated():
+        return redirect('/', **kwargs)
+    else:
+        if request.method == 'POST':
+            if 'remember_me' in request.POST:
+                request.session.set_expiry(1209600)  # 2 weeks
+            else:
+                request.session.set_expiry(0)
+        return login(request, **kwargs)
+
+
+def logout(request, next_page=None):
+    auth_logout(request)
+    if next_page:
+        return redirect(next_page)
+    return redirect('/')
