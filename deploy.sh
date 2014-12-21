285cd0eb0b6a5574ff648591ead70f73c86b0d90
Dipesh Acharya <xtranophilist@gmail.com>
2014-12-22 00:37:30 +0545

    improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 . ../env/bin/activate
 cd ../app/
-pip install -r requirements/production.txt
+pip install -r requirements/production.txt  | grep -v 'Requirement already satisfied' | grep -v 'Cleaning up...'
 ./manage.py collectstatic --noinput
-./manage.py migrate
+./manage.py migrate -v 0
 sudo supervisorctl restart muscn
