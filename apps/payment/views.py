b4c55d97da3ea0e7c9b69359e3f9ead3e498b714
Dipesh Acharya <xtranophilist@gmail.com>
2015-01-05 05:17:04 +0545

    approve and disprove for payment

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
 from muscn.utils.mixins import UpdateView
 from .models import Payment
@@ -14,12 +15,13 @@ class PaymentUpdateView(UpdateView):
     form_class = PaymentForm
     success_url = reverse_lazy('list_payments')
 
-
-# def list_payments(request):
-# all_payments = Payment.objects.all()
-# context = {
-#         'object_list': all_payments
-#     }
-#     return render(request, 'payment_list.html', context)
-
-
+    def get(self, request, *args, **kwargs):
+        if 'disprove' in request.GET:
+            obj = self.get_object()
+            obj.verified_by = None
+            obj.save()
+        if 'approve' in request.GET:
+            obj = self.get_object()
+            obj.verified_by = request.user
+            obj.save()
+        return super(PaymentUpdateView, self).get(request, *args, **kwargs)
