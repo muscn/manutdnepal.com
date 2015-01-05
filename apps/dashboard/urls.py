08e02753367dfcb6909244890a38b45c8de4c398
Dipesh Acharya <xtranophilist@gmail.com>
2015-01-06 02:48:16 +0545

    disabled creating payment w/o method

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
 
                        # TODO Move to payment app
                        url(r'^payment/$', payment_views.PaymentListView.as_view(), name='list_payments'),
-                       url(r'^payment/add/$', payment_views.PaymentCreateView.as_view(), name='create_payment'),
+                       # url(r'^payment/add/$', payment_views.PaymentCreateView.as_view(), name='create_payment'),
                        url(r'^payment/(?P<pk>\d+)/$', payment_views.PaymentUpdateView.as_view(), name='update_payment'),
                        url(r'^payment/(?P<pk>\d+)/delete/$', payment_views.PaymentDeleteView.as_view(),
                            name='delete_payment'),
