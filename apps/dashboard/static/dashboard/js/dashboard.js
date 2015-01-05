a30e439d5cd3fa284b2085315ced4dd95d4724ce
Dipesh Acharya <xtranophilist@gmail.com>
2015-01-05 21:54:02 +0545

    warning for buttons

--- apps/dashboard/static/dashboard/js/dashboard.js
+++ apps/dashboard/static/dashboard/js/dashboard.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
-            $(function () {
-                $('.flip-container').mouseenter(function () {
-                    $(this).addClass('open');
-                });
-                $('.flip-container').mouseleave(function () {
-                    $(this).removeClass('open');
-                });
-            });
+    $(function () {
 
+        $('.flip-container').mouseenter(function () {
+            $(this).addClass('open');
         });
+        $('.flip-container').mouseleave(function () {
+            $(this).removeClass('open');
+        });
+
+        $('.btn.warning').click(function (e) {
+            var action = e.currentTarget.value.toLowerCase();
+            if (confirm('Are you sure you want to ' + action + '?')) {
+                return true;
+            } else return false;
+        });
+        $('.btn-danger').click(function (e) {
+            if (confirm('Are you sure you want to delete?')) {
+                return true;
+            } else return false;
+        });
+
+
+    });
+});
