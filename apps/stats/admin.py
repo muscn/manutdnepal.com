87b77cf0252dfe56078a3b1dce5565961be670f7
Dipesh Acharya <xtranophilist@gmail.com>
2015-01-07 06:00:06 +0545

    Quote in admin

--- apps/stats/admin.py
+++ apps/stats/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Injury, Competition, CompetitionYear, City
+from .models import Injury, Competition, CompetitionYear, City, Quote
 
 admin.site.register(Injury)
 admin.site.register(Competition)
 admin.site.register(CompetitionYear)
 admin.site.register(City)
+admin.site.register(Quote)
