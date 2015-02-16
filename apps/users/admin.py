dee73479d8bac6250dc50bb3c6d103e0d6518fb7
Dipesh Acharya <xtranophilist@gmail.com>
2015-02-16 08:41:09 +0545

    admin filters for memberships

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin, UserChangeForm as DjangoUserChangeForm, \
     UserCreationForm as DjangoUserCreationForm
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from apps.users.models import User, GroupProxy, Membership
 
@@ -93,6 +96,49 @@ class CustomUserAdmin(UserAdmin):
     )
 
 
+class DecadeBornListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('decade born')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'decade'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('80s', _('in the eighties')),
+            ('90s', _('in the nineties')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == '80s':
+            return queryset.filter(date_of_birth__gte=date(1980, 1, 1),
+                                   date_of_birth__lte=date(1989, 12, 31))
+        if self.value() == '90s':
+            return queryset.filter(date_of_birth__gte=date(1990, 1, 1),
+                                   date_of_birth__lte=date(1999, 12, 31))
+
+
+class MembershipAdmin(admin.ModelAdmin):
+    list_filter = ('gender', 'present_status', 'shirt_size', DecadeBornListFilter)
+
+
+admin.site.register(Membership, MembershipAdmin)
+
 admin.site.register(User, CustomUserAdmin)
 
 # Removing default apps
@@ -103,4 +149,4 @@ from django.contrib.auth.models import Group
 
 admin.site.unregister(Group)
 admin.site.register(GroupProxy)
-admin.site.register(Membership)
+
