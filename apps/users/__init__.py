c0c1d55354b2d3144008bb4458fff9324943bebe
Dipesh Acharya <xtranophilist@gmail.com>
2015-02-25 10:55:13 +0545

    enable esewa settings

--- apps/users/__init__.py
+++ apps/users/__init__.py
@@ -3,6 +3,7 @@ import dbsettings
 
 class MembershipSetting(dbsettings.Group):
     membership_fee = dbsettings.FloatValue('Membership Fee')
+    enable_esewa = dbsettings.BooleanValue()
 
 
 membership_settings = MembershipSetting('Membership Settings')
