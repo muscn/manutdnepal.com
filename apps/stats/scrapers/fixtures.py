db2ea9dcec37487c94ccdcc07413dc2a0c0690aa
Dipesh Acharya <xtranophilist@gmail.com>
2015-08-19 20:14:31 +0545

    Scraping of Fixtures

--- apps/stats/scrapers/fixtures.py
+++ apps/stats/scrapers/fixtures.py
@@ -1,26 +1,59 @@
 import urllib
 import json
+from ics import Calendar
+from urllib2 import urlopen
+from .base import Scraper
+from apps.stats.models import Fixture, CompetitionYear, Competition, Team
 
-class FixturesScraper():
+
+class FixturesScraper(Scraper):
+    # @classmethod
+    # def start(cls):
+    #     from django.conf import settings
+    #
+    #     api_key = settings.FOOTBALL_API_KEY
+    #     # TODO make year dynamic
+    #     url = 'http://football-api.com/api/?Action=fixtures&APIKey=' + api_key + '&comp_id=1204&from_date=01.08.2015&to_date=31.05.2016'
+    #     print 'Retrieving from API'
+    #     f = urllib.urlopen(url)
+    #     data = json.loads(f.read())
+    #     # from django.core.cache import cache
+    #     # cache.set('fixtures', data, timeout=None)
+    #     # data = cache.get('fixtures')
+    #     # if not data['ERROR'] == 'OK':
+    #     #     print 'Error: ' + data['ERROR']
+    #     # for match in data['matches']:
+
     @classmethod
     def start(cls):
-        from django.conf import settings
-
-        api_key = settings.FOOTBALL_API_KEY
-        # TODO make year dynamic
-        url = 'http://football-api.com/api/?Action=fixtures&APIKey=' + api_key + '&comp_id=1204&from_date=01.08.2015&to_date=31.05.2016'
-        print 'Retrieving from API'
-        f = urllib.urlopen(url)
-        data = json.loads(f.read())
-        # from django.core.cache import cache
-        # cache.set('fixtures', data, timeout=None)
-        # data = cache.get('fixtures')
-        # if not data['ERROR'] == 'OK':
-        #     print 'Error: ' + data['ERROR']
-        import ipdb
-
-        ipdb.set_trace()
-
-        # for match in data['matches']:
+        url = 'http://calendar.manutd.com/Manchester_United.ics'
+        year = '2015'
+        # url = 'http://hackeragenda.urlab.be/events/events.ics'
+        cal_content = cls.get_url_content(url)
+        cal_content = cal_content.replace('RATE:', 'RDATE:')
+        cal = Calendar(cal_content.decode('iso-8859-1'))
+        Fixture.get_upcoming().delete()
+        for event in cal.events:
+            fixture = Fixture()
+            splits = event.name.split('-')
+            competition_name = splits[1].strip()
+            if competition_name == u'English Barclays Premier League':
+                competition = Competition.objects.get(name='English Premier League')
+            competition_year = CompetitionYear.objects.get(competition=competition, year=year)
+            fixture.competition_year = competition_year
+            if splits[0][:17] == 'Manchester United':
+                fixture.is_home_game = True
+                opponent_team_name = splits[0][21:].strip()
+            else:
+                fixture.is_home_game = False
+                opponent_team_name = splits[0].strip()[:-21]
+            try:
+                opponent_team = Team.get(opponent_team_name)
+            except Team.DoesNotExist:
+                raise Exception('Please add a team with name : ' + opponent_team_name)
+            fixture.opponent = opponent_team
+            fixture.datetime = event.begin.datetime
+            fixture.venue = event.location.split('-')[1].strip()
+            fixture.save()
 
 
--- apps/stats/scrapers/teams.py
+++ apps/stats/scrapers/teams.py
@@ -5,14 +5,13 @@ from apps.stats.models import Team
 class TeamsScraper(Scraper):
     @classmethod
     def start(cls):
-        # cls.fetch_from_espn_data()
+        cls.fetch_from_espn_data()
         cls.fetch_all_teams()
-
 
     @classmethod
     def fetch_from_espn_data(cls):
         print 'Retrieving from ESPN EPL Teams Data JSON'
-        url = 'https://raw.githubusercontent.com/jokecamp/FootballData/master/epl-teams-2013-2014.json'
+        url = 'https://raw.githubusercontent.com/jokecamp/FootballData/master/EPL%201992%20-%202015/epl-teams-2013-2014.json'
         espn_data = cls.get_json_from_url(url)
         teams = espn_data['sports'][0]['leagues'][0]['teams']
         for datum in teams:
@@ -25,12 +24,12 @@ class TeamsScraper(Scraper):
     @classmethod
     def fetch_all_teams(cls):
         print 'Fetching all teams from compiled json'
-        url ='https://raw.githubusercontent.com/jokecamp/FootballData/master/teams.json'
+        url = 'https://raw.githubusercontent.com/jokecamp/FootballData/master/openFootballData/teams.json'
         data = cls.get_json_from_url(url)
         for datum in data[222:]:
             team, created = Team.objects.get_or_create(name=datum['title'])
             team.short_name = datum['code']
-            team.alternative_names = datum['synonyms']
+            if datum['synonyms']:
+                team.alternative_names = '|' + datum['synonyms'] + '|'
             # if team.short_name:
             team.save()
-            
