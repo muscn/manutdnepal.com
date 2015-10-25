a79eb30b2169f6386179b07d41e899af130972ce
Dipesh Acharya <xtranophilist@gmail.com>
2015-10-25 22:01:29 +0545

    Add other channels

--- apps/stats/scrapers/tv.py
+++ apps/stats/scrapers/tv.py
@@ -6,11 +6,23 @@ import pytz
 
 
 class TVScraper(Scraper):
-    channels = {'star-sports-4-india': 'Star Sports 4', 'star-sports-india':'Star Sports 1', 'ten-sports': 'TEN Sports'}
+    channels = {'star-sports-india': 'Star Sports 1',
+                'star-sports-2-india': 'Star Sports 2',
+                'star-sports-3-india': 'Star Sports 3',
+                'star-sports-4-india': 'Star Sports 4',
+                'star-sports-hd-1-india': 'Star Sports HD 1',
+                'star-sports-hd-2-india': 'Star Sports HD 2',
+                'sony-six-india': 'Sony Six',
+                'sony-six-hd-india': 'Sony Six HD',
+                'sony-kix-india': 'Sony Kix',
+                'ten-sports': 'TEN Sports',
+                'ten-action': 'Ten Action',
+                }
 
     @classmethod
     def scrape(cls):
-        json_data = cls.get_json_from_url('http://www.livesoccertv.com/m/api/teams/england/manchester-united/?iso_code=np')
+        json_data = cls.get_json_from_url(
+            'http://www.livesoccertv.com/m/api/teams/england/manchester-united/?iso_code=np')
         if 'fixtures' in json_data:
             for fixture in json_data['fixtures']:
                 channels = fixture.get('channels')
