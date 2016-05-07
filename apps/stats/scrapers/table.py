65e4746ff77be613bfc66134d4aa2af173209336
Dipesh Acharya <xtranophilist@gmail.com>
2016-05-07 21:19:33 +0545

    Fix table scraper

--- apps/stats/scrapers/table.py
+++ apps/stats/scrapers/table.py
@@ -1,5 +1,3 @@
-import json
-
 from django.core.cache import cache
 
 from .base import Scraper
@@ -24,14 +22,17 @@ class TableScraper(Scraper):
             data['position'] = cols[1].cssselect('span')[1].text_content()
             data['name'] = cols[2].text_content()
             data['p'] = cols[3].text_content()
-            data['w'] = cols[3].text_content()
-            data['d'] = cols[3].text_content()
-            data['l'] = cols[3].text_content()
-            data['f'] = cols[3].text_content()
-            data['a'] = cols[3].text_content()
-            data['gd'] = cols[3].text_content()
-            data['pts'] = cols[3].text_content()
+            data['w'] = cols[4].text_content()
+            data['d'] = cols[5].text_content()
+            data['l'] = cols[6].text_content()
+            data['f'] = cols[7].text_content()
+            data['a'] = cols[8].text_content()
+            data['gd'] = cols[9].text_content()
+            data['pts'] = cols[10].text_content()
             cls.data['teams'].append(data)
+
+        # Also fetch all matches this week
+        
 
     @classmethod
     def save(cls):
