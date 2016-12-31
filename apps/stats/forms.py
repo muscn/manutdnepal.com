7a9a45d400e4049ec242bd9e5c31c2d7b99a2124
Dipesh Acharya <xtranophilist@gmail.com>
2016-12-31 17:05:44 +0545

    Fix fixture form

--- apps/stats/forms.py
+++ apps/stats/forms.py
@@ -29,7 +29,7 @@ class ResultForm(HTML5BootstrapModelForm):
 class FixtureForm(HTML5BootstrapModelForm):
     class Meta:
         model = Fixture
-        exclude = ('competition_year', 'opponent', 'datetime', 'venue')
+        fields = ('round', 'broadcast_on', 'remarks')
 
 
 class GoalForm(HTML5BootstrapModelForm):
