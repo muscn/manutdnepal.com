2978586cd4f09959ceb41ea0697c1bb7d6538169
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-02-10 11:34:53 +0545

    regex Manchester United \((.*)\) -> Manchester United\s* \((.*)\) in injuries scrape, no_of_injuries

--- apps/stats/scrapers/injuries.py
+++ apps/stats/scrapers/injuries.py
@@ -11,41 +11,42 @@ class InjuriesScraper(Scraper):
     @classmethod
     def scrape(cls):
         root = cls.get_root_tree()
-        united_row = root.xpath('//a[contains(text(), "Manchester United")]')[1].getparent().getparent().getparent()
-        urtc = united_row.text_content().strip()
-        no_of_injuries = int(re.match(r'Manchester United \((.*)\)', urtc, re.M | re.I).group(1))
-        injury_rows = united_row.xpath('./following-sibling::tr')[:no_of_injuries]
-        for row in injury_rows:
-            player_link = row.getchildren()[0].getchildren()[1].attrib['href']
-            player_name = player_link.split('/')[-1:][0].split('.')[0].replace('_injury', '').replace('_', ' ').title()
-            try:
-                player = Player.objects.get(name=player_name)
-            except Player.DoesNotExist:
-                player_name_short = row.getchildren()[0].text_content()
-                player_name_last = player_name_short.split()[-1:][0]
+        if root:
+            united_row = root.xpath('//a[contains(text(), "Manchester United")]')[1].getparent().getparent().getparent()
+            urtc = united_row.text_content().strip()
+            no_of_injuries = int(re.match(r'Manchester United\s* \((.*)\)', urtc, re.M | re.I).group(1))
+            injury_rows = united_row.xpath('./following-sibling::tr')[:no_of_injuries]
+            for row in injury_rows:
+                player_link = row.getchildren()[0].getchildren()[1].attrib['href']
+                player_name = player_link.split('/')[-1:][0].split('.')[0].replace('_injury', '').replace('_', ' ').title()
                 try:
-                    player = Player.objects.get(name__icontains=player_name_last)
-                except MultipleObjectsReturned:
-                    player_name_first = player_name_short.split()[0]
-                    player = Player.objects.get(name__icontains=player_name_last, name__startswith=player_name_first)
+                    player = Player.objects.get(name=player_name)
                 except Player.DoesNotExist:
-                    raise ValueError('Player "%s" does\'nt exist.' % (player_name_last))
-            cls.data[player] = {'type': row.getchildren()[1].text_content()}
-            if row.getchildren()[2].text:
-                cls.data[player]['remarks'] = row.getchildren()[2].text
-            return_date_raw = row.getchildren()[3].text
-            if return_date_raw.lower() == 'no return date':
-                pass
-            elif len(return_date_raw.split()) == 3:
-                day = return_date_raw.split()[1].strip(',').zfill(2)
-                month = return_date_raw.split()[0]
-                year = return_date_raw.split()[2]
-                cls.data[player]['return_date'] = datetime.datetime.strptime(day + '-' + month + '-' + year, '%d-%B-%Y')
-            elif len(return_date_raw.split()) == 2:
-                day = '15'
-                month = return_date_raw.split()[0]
-                year = return_date_raw.split()[1]
-                cls.data[player]['return_date'] = datetime.datetime.strptime(day + '-' + month + '-' + year, '%d-%B-%Y')
+                    player_name_short = row.getchildren()[0].text_content()
+                    player_name_last = player_name_short.split()[-1:][0]
+                    try:
+                        player = Player.objects.get(name__icontains=player_name_last)
+                    except MultipleObjectsReturned:
+                        player_name_first = player_name_short.split()[0]
+                        player = Player.objects.get(name__icontains=player_name_last, name__startswith=player_name_first)
+                    except Player.DoesNotExist:
+                        raise ValueError('Player "%s" does\'nt exist.' % (player_name_last))
+                cls.data[player] = {'type': row.getchildren()[1].text_content()}
+                if row.getchildren()[2].text:
+                    cls.data[player]['remarks'] = row.getchildren()[2].text
+                return_date_raw = row.getchildren()[3].text
+                if return_date_raw.lower() == 'no return date':
+                    pass
+                elif len(return_date_raw.split()) == 3:
+                    day = return_date_raw.split()[1].strip(',').zfill(2)
+                    month = return_date_raw.split()[0]
+                    year = return_date_raw.split()[2]
+                    cls.data[player]['return_date'] = datetime.datetime.strptime(day + '-' + month + '-' + year, '%d-%B-%Y')
+                elif len(return_date_raw.split()) == 2:
+                    day = '15'
+                    month = return_date_raw.split()[0]
+                    year = return_date_raw.split()[1]
+                    cls.data[player]['return_date'] = datetime.datetime.strptime(day + '-' + month + '-' + year, '%d-%B-%Y')
 
     @classmethod
     def save(cls):
