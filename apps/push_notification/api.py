e1f5c519961b6740c6017febbb7af3dcc7c259f3
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-03-08 13:47:04 +0545

    reg_id unique error handle

--- apps/push_notification/api.py
+++ apps/push_notification/api.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,7 +17,7 @@ class UserDeviceViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets
         try:
             obj, created = UserDevice.objects.get_or_create(dev_id=params.get('dev_id'))
             obj.name = params.get('name')
-            obj.reg_id = params.get('reg_id')
+            obj.reg_id = params.get('reg_id') or None
             obj.device_type = params.get('type')
             # TODO UserDevice set User
             # obj.user = request.user
@@ -24,4 +25,5 @@ class UserDeviceViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets
             obj.save()
             return Response(self.serializer_class(obj).data, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({'error': e}, status=status.HTTP_400_BAD_REQUEST)
+            obj.delete()
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
