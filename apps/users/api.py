64b0cf2ea034076d06f434f13ec67cb9b388de7b
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-05-24 16:46:32 +0545

    CustomObtainAuth copy from ObtainAuthToken

--- apps/users/api.py
+++ apps/users/api.py
@@ -5,6 +5,11 @@ from apps.key.permissions import DistributedKeyAuthentication
 from apps.stats.serializers import FixtureSerializer
 from apps.users.models import User
 from apps.users.serializers import UserSerializer
+from rest_framework import parsers, renderers
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -27,5 +32,19 @@ class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
         return Response(self.serializer_class(user).data, status=status.HTTP_200_OK)
 
 
-class CustomObtainAuth(ObtainAuthToken):
+class CustomObtainAuth(APIView):
+    throttle_classes = ()
     permission_classes = (DistributedKeyAuthentication,)
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+obtain_auth_token = ObtainAuthToken.as_view()
