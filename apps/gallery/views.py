3751c9a966a09d827e6e07394d268d50bd658262
Dipesh Acharya <xtranophilist@gmail.com>
2017-05-27 17:33:55 +0545

    Add multi upload support form images in albums

--- apps/gallery/views.py
+++ apps/gallery/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+import os
+
+from django.conf import settings
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.decorators.http import require_POST
 from django.views.generic import DetailView
 
-from .models import Album
+from apps.gallery.http import upload_receive, UploadResponse, JFUResponse
+from .models import Album, Image
 
 
 def album_list(request):
@@ -11,3 +18,39 @@ def album_list(request):
 
 class AlbumDetail(DetailView):
     model = Album
+
+
+@staff_member_required
+@require_POST
+def multi_upload(request, pk):
+    if request.method == 'POST':
+        file = upload_receive(request)
+        instance = Image(file=file, album_id=pk)
+        instance.save()
+        basename = os.path.basename(instance.file.path)
+        file_dict = {
+            'name': basename,
+            'size': file.size,
+
+            'url': settings.MEDIA_URL + basename,
+            'thumbnailUrl': instance.file.url,
+
+            'deleteUrl': reverse('image-delete', kwargs={'pk': instance.pk}),
+            'deleteType': 'POST',
+        }
+        return UploadResponse(request, file_dict)
+    return render(request, 'upload.html')
+
+
+@require_POST
+@staff_member_required
+def upload_delete(request, pk):
+    success = True
+    try:
+        instance = Image.objects.get(pk=pk)
+        os.unlink(instance.file.path)
+        instance.delete()
+    except Image.DoesNotExist:
+        success = False
+
+    return JFUResponse(request, success)
