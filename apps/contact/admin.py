477dff4646b8b668b395fe3355b2dbfc04ff50da
Dipesh Acharya <xtranophilist@gmail.com>
2017-06-03 16:23:06 +0545

    Improve contact models and admin

--- apps/contact/admin.py
+++ apps/contact/admin.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from solo.admin import SingletonModelAdmin
+from .models import ContactSetting, Message
+
+admin.site.register(ContactSetting, SingletonModelAdmin)
+
+
+class MesageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at', 'message')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'email', 'message')
+
+
+admin.site.register(Message, MesageAdmin)
--- apps/contact/models.py
+++ apps/contact/models.py
@@ -7,14 +7,15 @@ from django.db import models
 from froala_editor.fields import FroalaField
 from solo.models import SingletonModel
 
+from muscn import settings
 from muscn.utils.location import LocationField
 
 
-class Contact(SingletonModel):
+class ContactSetting(SingletonModel):
     content = FroalaField(null=True, blank=True)
-    email = ArrayField(models.EmailField(blank=True, null=True))
-    phone = ArrayField(models.CharField(max_length=50, blank=True, null=True))
-    fax = ArrayField(models.CharField(max_length=50, blank=True, null=True))
+    email = ArrayField(models.EmailField(blank=True, null=True), blank=True, null=True)
+    phone = ArrayField(models.CharField(max_length=50, blank=True, null=True), blank=True, null=True)
+    fax = ArrayField(models.CharField(max_length=50, blank=True, null=True), blank=True, null=True)
     facebook_page = models.URLField(blank=True, null=True)
     facebook_group = models.URLField(blank=True, null=True)
     twitter = models.URLField(blank=True, null=True)
@@ -27,8 +28,10 @@ class Contact(SingletonModel):
 
 class Message(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
-    email = models.EmailField(max_length=255)
-    message = models.TextField(blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    message = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return 'Message from %s' % self.name
