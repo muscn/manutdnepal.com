14e5900bb843e08759123e63afc0650701fa8225
Dipesh Acharya <xtranophilist@gmail.com>
2017-06-03 15:21:30 +0545

    Optimize football team page

--- apps/team/views.py
+++ apps/team/views.py
@@ -3,8 +3,8 @@ from apps.team.models import Player, Staff
 
 
 def football_team(request):
-    players = Player.objects.filter(active=True)
-    staffs = Staff.objects.filter(active=True)
+    players = Player.objects.filter(active=True).select_related('user__membership')
+    staffs = Staff.objects.filter(active=True).select_related('user__membership')
     context = {
         'players': players,
         'staffs': staffs,
