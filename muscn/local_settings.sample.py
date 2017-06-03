859c37a689ad5fa3732c03d1ea4c2b11f898a80d
Dipesh Acharya <xtranophilist@gmail.com>
2017-06-03 20:36:34 +0545

    Refactor local settings sample

--- muscn/local_settings.sample.py
+++ muscn/local_settings.sample.py
@@ -1,60 +1,67 @@
-from settings import *
+import os
+from .settings import BASE_DIR
 
-import sys
-
-DEBUG = False
-ALLOWED_HOSTS = ['manutd.org.np', '127.0.0.1', 'localhost']
-SITE_ID = 1
-SECRET_KEY = '<35=0kv-7q5$otz58g^fv&o)iq&hldz60p^6%86xui%qcd1234'
-#TEMPLATE_DEBUG = DEBUG
-
-ADMINS = ()
-MANAGERS = ()
-
-
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'muscn',
-         'USER': 'muscn',
-         'PASSWORD': 'xxx',
-         'HOST': 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-         'PORT': '',  # Set to empty string for default.
-     }
-}
-
+# Local
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 MEDIA_URL = '/media/'
+ALLOWED_HOSTS = ['manutd.org.np', '127.0.0.1', 'localhost']
 
-WEBHOOK_PASSCODE = 'password'
-
-INSTALLED_APPS += (
-    # 'debug_toolbar',
-)
-
-CACHES = {
+# Secrets
+SECRET_KEY = '<35=0kv-7q5$otz58g^fv&o)iq&hldz60p^6%86xui%qcd1234'
+ADMINS = ()
+MANAGERS = ()
+DATABASES = {
     'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': '127.0.0.1:6379',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'muscn',
+        'USER': 'muscn',
+        'PASSWORD': 'xxx',
+        'HOST': 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',  # Set to empty string for default.
     }
 }
-
-
+WEBHOOK_PASSCODE = 'password'
 FOOTBALL_API_KEY = 'xxx'
-TEMPLATES[0]['OPTIONS']['debug'] = True
-
-SERVER_EMAIL = 'manutd@awecode.com'
 FB_ACCESS_TOKEN = 'xxx'
 FCM_APIKEY = 'xxx'
-
 OPBEAT = {
     'ORGANIZATION_ID': 'adsd',
     'APP_ID': 'appt_id',
     'SECRET_TOKEN': 'asd',
 }
-
 ANYMAIL = {
     "MAILGUN_API_KEY": "<your Mailgun key>",
 }
+
+# Development
+# from .settings import INSTALLED_APPS, MIDDLEWARE, TEMPLATES
+#
+# DEBUG = True
+# TEMPLATES[0]['OPTIONS']['debug'] = True
+# INSTALLED_APPS += (
+#     'debug_toolbar',
+# )
+# DEBUG_TOOLBAR_CONFIG = {
+#     'RESULTS_CACHE_SIZE': 100
+# }
+# MIDDLEWARE += (
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# )
+# INTERNAL_IPS = ['127.0.0.1']
+# DEBUG_TOOLBAR_PANELS = [
+#     'debug_toolbar.panels.versions.VersionsPanel',
+#     'debug_toolbar.panels.timer.TimerPanel',
+#     'debug_toolbar.panels.settings.SettingsPanel',
+#     'debug_toolbar.panels.headers.HeadersPanel',
+#     'debug_toolbar.panels.request.RequestPanel',
+#     'debug_toolbar.panels.sql.SQLPanel',
+#     'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+#     # 'debug_toolbar.panels.templates.TemplatesPanel',
+#     'debug_toolbar.panels.cache.CachePanel',
+#     'debug_toolbar.panels.signals.SignalsPanel',
+#     'debug_toolbar.panels.logging.LoggingPanel',
+#     'debug_toolbar.panels.redirects.RedirectsPanel',
+# ]
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
