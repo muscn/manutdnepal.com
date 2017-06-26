f3d1d9510c6c9ab9b10736446bf80faec9435c5a
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-06-26 15:40:20 +0545

    mf

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -6,7 +6,7 @@ class MembershipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Membership
         exclude = ('status', 'homepage', 'user', 'registration_date', 'approved_date', 'approved_by', 'payment', 'expiry_date')
-
+        # fields = ('date_of_birth', 'gender', 'permanent_address', 'mobile', 'identification_file',)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
