0713b139c9970b77aa0073c1630c7a6bf4db688f
Dipesh Acharya <xtranophilist@gmail.com>
2017-09-12 13:59:48 +0545

    Use django-redis instead of django-redis-cache

--- muscn/settings.py
+++ muscn/settings.py
@@ -147,13 +147,18 @@ CSRF_COOKIE_NAME = 'ct'
 CSRF_COOKIE_SECURE = True
 
 CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': '127.0.0.1:6379',
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
 SOLO_CACHE = 'default'
 
 LOGGING = {
