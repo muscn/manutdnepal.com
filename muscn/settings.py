1b013c25641f46afa10ef4940eeaedac6ed7ee1d
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-09-22 16:01:49 +0545

    mf

--- muscn/settings.py
+++ muscn/settings.py
@@ -103,7 +103,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 25,
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
         'apps.key.permissions.DistributedKeyAuthentication'
 
     ],
