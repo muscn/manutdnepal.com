56c852aa3b5c4fa1341beab5e6422952b0c77e41
Dipesh Acharya <xtranophilist@gmail.com>
2017-09-24 15:01:48 +0545

    Fix dashboard index

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,2 +1,21 @@
 import "./js/_bootstrap"
 import "./scss/dashboard.scss";
+
+$(document).ready(function () {
+    $(function () {
+
+        $('.flip-container').mouseenter(function () {
+            $(this).addClass('open');
+        });
+        $('.flip-container').mouseleave(function () {
+            $(this).removeClass('open');
+        });
+
+        $('.btn.warning, .btn-danger').click(function (e) {
+            var action = e.currentTarget.value.toLowerCase();
+            if (confirm('Are you sure you want to ' + action + '?')) {
+                return true;
+            } else return false;
+        });
+    });
+});
