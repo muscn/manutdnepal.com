765f1d460feac0b9ea9bf0ead7b15276f0aae6ff
Dipesh Acharya <xtranophilist@gmail.com>
2017-09-26 22:32:44 +0545

    Fix partner lookup

--- apps/partner/views.py
+++ apps/partner/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from apps.partner.models import Partner
 
 
 def view_partner(request, slug):
-    partner = Partner.objects.get(slug=slug)
+    partner = get_object_or_404(Partner, slug=slug)
     return render(request, 'partner/view_partner.html', {'partner': partner})
