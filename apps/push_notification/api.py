59ce0b9256c4f0c9839e7c86289a28c4d46be9f8
Dipesh Acharya <xtranophilist@gmail.com>
2017-09-26 21:49:26 +0545

    Do not try to save user device if reg_id isn't received

--- apps/push_notification/api.py
+++ apps/push_notification/api.py
@@ -1,5 +1,6 @@
 from django.db import IntegrityError
 from rest_framework import viewsets, mixins
+from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework import status
 from ..key.permissions import DistributedKeyAuthentication
@@ -14,8 +15,12 @@ class UserDeviceViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets
     def create(self, request, *args, **kwargs):
         params = request.data
         user = request.user if request.user.is_authenticated else None
-        obj, created = UserDevice.objects.update_or_create(dev_id=params.get('dev_id'),
-                                                        defaults={'name': params.get('name'), 'reg_id': params.get('reg_id'),
-                                                                  'device_type': params.get('type'), 'is_active': True,
-                                                                  'user': user})
-        return Response(self.serializer_class(obj).data, status=status.HTTP_200_OK)
+        reg_id = params.get('reg_id')
+        if reg_id:
+            obj, created = UserDevice.objects.update_or_create(dev_id=params.get('dev_id'),
+                                                               defaults={'name': params.get('name'), 'reg_id': reg_id,
+                                                                         'device_type': params.get('type'), 'is_active': True,
+                                                                         'user': user})
+            return Response(self.serializer_class(obj).data, status=status.HTTP_200_OK)
+        else:
+            raise APIException('No registration ID.')
