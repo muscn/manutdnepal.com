a619e1b3a7042a52f33b7f34e71c6c9139ac59a5
Dipesh Acharya <xtranophilist@gmail.com>
2017-09-27 12:04:59 +0545

    Make users active by default

--- apps/users/models.py
+++ apps/users/models.py
@@ -93,7 +93,7 @@ class User(AbstractBaseUser):
     status = models.CharField(choices=USER_STATUSES, max_length=30, default='Registered')
     email = models.EmailField(verbose_name='Email address', max_length=254, unique=True, db_index=True)
 
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     groups = models.ManyToManyField(Group, related_name='users', blank=True)
