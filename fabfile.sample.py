b9592371cf2f3f829f0b1b29d05db18a5a6ab65d
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-02 22:04:02 +0545

    Refactor fabfile

--- fabfile.sample.py
+++ fabfile.sample.py
@@ -1,17 +1,10 @@
 from fabric.context_managers import cd, prefix, settings, lcd
-from fabric.contrib import django
 from fabric.decorators import task, roles
 from fabric.api import local
 from fabric.operations import run, sudo
 from fabric.state import env
 from fabric.tasks import execute
 from contextlib import contextmanager as _contextmanager
-
-django.settings_module('muscn.settings')
-
-from django.conf import settings as dj_settings
-
-DATABASES = dj_settings.DATABASES
 
 env.user = 'user'
 env.roledefs = {
@@ -38,18 +31,27 @@ def virtualenv(env_path):
             yield
 
 
-def remove_lines(text, matches):
-    if type(matches) is not list:
-        matches = [matches]
-    lines = text.splitlines()
-    for match in matches:
-        lines = [line for line in lines if match not in line]
-    return "\n".join(lines)
-
-
 @task
 def test():
     local("./manage.py test -v 2 --keepdb")
+
+
+@task
+def webpack_compile():
+    local('NODE_ENV=production webpack -p')
+
+
+@task
+@roles('prod')
+def sync_static():
+    local("rsync -avz -e 'ssh -p 22' %s %s@%s:%s" % (
+        paths['project_dir'] + 'static/', env.user, env.roledefs.get('cdn')[0], paths['remote_static']))
+
+
+@task
+def collect_static():
+    with virtualenv(paths['project_dir'] + 'env/') and lcd(paths['project_dir'] + paths['project_name']):
+        local('DJANGO_ENV=production python manage.py collectstatic --noinput')
 
 
 @task
@@ -65,26 +67,8 @@ def flush_pagespeed_cache():
 
 
 @task
-def webpack_compile():
-    local('webpack -p')
-
-
-# fantasysport/static/dist
-@task
-@roles('prod')
-def sync_static():
-    local("rsync -avz -e 'ssh -p 22' %s %s@%s:%s" % (
-        paths['project_dir'] + 'static/', env.user, env.roledefs.get('cdn')[0], paths['remote_static']))
-
-
-@task
-def collect_static():
-    with virtualenv(paths['project_dir'] + 'env/') and lcd(paths['project_dir'] + paths['project_name']):
-        local('python manage.py collectstatic --noinput')
-
-
-@task
-def deploy_prod():
+def deploy():
+    local("git checkout master")
     # Because we don't want to run `webpack -p` on server, neither keep dist on VCS
     execute(webpack_compile)
     execute(sync_static)
@@ -94,25 +78,11 @@ def deploy_prod():
 
 
 @task
-@roles('prod')
-def manage_prod(command):
-    with virtualenv(paths['prod_env']), cd(paths['prod_app']), settings(warn_only=True):
-        run('./manage.py {}'.format(command))
-
-
-@task
-def deploy():
-    local("git checkout master")
-    execute(deploy_prod)
+def backend_deploy():
+    execute(push_prod)
 
 
 @task
 @roles(hosts)
 def uptime():
     run('uptime')
-    
-@task
-def backend_deploy():
-    local("git checkout master")
-    execute(push_prod)
-
