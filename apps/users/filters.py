15643a986888660d573bf0dab872adc369d6cb3f
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-23 13:28:20 +0545

    Reorder filters

--- apps/users/filters.py
+++ apps/users/filters.py
@@ -20,5 +20,5 @@ class UserFilter(django_filters.FilterSet):
 
     class Meta:
         model = User
-        fields = ['status', 'card_statuses__status', 'card_statuses__pickup_location', 'card_statuses__season']
+        fields = ['status', 'payment_method', 'card_statuses__status', 'card_statuses__pickup_location', 'card_statuses__season']
         form = BootstrapForm
