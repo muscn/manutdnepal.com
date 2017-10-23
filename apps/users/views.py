87367b7378d31f0b667167306d6b632eb4b465b5
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-23 20:06:36 +0545

    mf

--- apps/users/views.py
+++ apps/users/views.py
@@ -244,7 +244,7 @@ class UserListView(StaffOnlyMixin, ListView):
         if 'q' in self.request.GET:
             q = self.request.GET['q']
             self.queryset = User.objects.filter(
-                Q(id=q) |
+                Q(id__icontains=q) |
                 Q(username__icontains=q) |
                 Q(full_name__icontains=q) |
                 Q(email__icontains=q) |
